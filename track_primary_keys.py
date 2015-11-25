07b0cef65df3130f0a0e70306931a32b89013945
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2015-11-25 17:30:13 -0500

    Store all composite primary keys in MySQL

---
+++ track_primary_keys.py
@@ -0,0 +1,10 @@
+import pymysql
+from pymongo import MongoClient
+con = pymysql.connect(host='localhost', unix_socket='/tmp/mysql.sock',user='root', passwd="", db='NBA')
+mysql = con.cursor(pymysql.cursors.DictCursor)
+client = MongoClient()
+db = client.sportsvu
+primary_keys = db.requests.distinct('_id')
+for primary_key in primary_keys:
+	mysql.execute("""insert into sportsvu_mongodb_primary_keys(game_id, event_id) values("{game_id}","{event_id}")""".format(game_id=primary_key['game_id'],event_id=primary_key['event_id']))
+	con.commit()
