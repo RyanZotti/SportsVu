6f675b0c725874f266444353c4ac0de628123ec4
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2015-12-06 13:32:39 -0500

    Cleanup script now capable of running standalone

--- mongo_cleanup.py
+++ mongo_cleanup.py
@@ -5,12 +5,25 @@ con = pymysql.connect(host='localhost', unix_socket='/tmp/mysql.sock',user='root
 mysql = con.cursor(pymysql.cursors.DictCursor)
 client = MongoClient()
 db = client.sportsvu
-mysql.execute("select game_id, event_id from sportsvu_cleanup")
+# Clean up entries from any previous runs
+mysql.execute("delete from sportsvu_mongodb_primary_keys")
+con.commit()
+primary_keys = db.requests.find({},{'_id':1})
+# Store all game_id and event_ids
+for primary_key in primary_keys:
+	game_id = primary_key['_id']['game_id']
+	event_id = primary_key['_id']['event_id']
+	mysql.execute("""insert into sportsvu_mongodb_primary_keys(game_id, event_id) values("{game_id}","{event_id}")""".format(game_id=game_id,event_id=event_id))
+	con.commit()
+# Delete incomplete game_id, event_id combinations that aren't recorded as stored in the SportsVuGameIdsStoredInMongo table
+mysql.execute('''
+	select game_id, event_id from sportsvu_mongodb_primary_keys where not exists (select * from SportsVuGameIdsStoredInMongo where SportsVuGameIdsStoredInMongo.game_id = sportsvu_mongodb_primary_keys.game_id)''')
 for row in mysql.fetchall():
 	composite_key = collections.OrderedDict()
 	composite_key["game_id"]=str(row['game_id'])
 	composite_key["event_id"]=int(row['event_id'])
 	db.requests.delete_one({'_id': composite_key})
+print('Finished')
 
 
 
